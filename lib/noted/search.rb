8848fd1c3fd9b72488d31c253d9006c9c7c5f49f
Jeffrey Horn <jeffh@generalassemb.ly>
2015-03-08 00:18:15 -0500

    Add search by filename and content

--- lib/noted/search.rb
+++ lib/noted/search.rb
@@ -1,5 +1,21 @@
 module Noted
   module Search
+    extend self
 
+    def perform(query)
+      (by_file_name(query).compact + by_full_text(query).compact).flatten
+    end
+
+    def by_file_name(query)
+      Noted.cache.files.select do |file|
+        File.fnmatch("#{query}*", file.split('/')[-1])
+      end
+    end
+
+    def by_full_text(query)
+      Noted.cache.files.select do |file|
+        File.open(file) { |f| f.any? { |line| line.include? query } }
+      end
+    end
   end
 end
--- spec/noted/search_spec.rb
+++ spec/noted/search_spec.rb
@@ -1,27 +1,33 @@
 require 'noted/search'
+require 'support/example_configuration'
 
 describe Noted::Search do
+  include_context 'example_configuration'
+
   describe '#perform' do
-    before(:all) do
-      Noted.configure do |config|
-        config.path = [
-          File.expand_path('../../fixtures/foo_dir', __FILE__ ),
-          File.expand_path('../../fixtures/bar_dir', __FILE__ )
-        ]
-      end
-    end
-
     it 'searches note paths for matching files' do
+    end
 
+    it 'supports file globs' do
+      expect(Noted::Search.perform('*')).to eq all_files
     end
   end
 
-  describe '#file_names' do
-    it 'finds file names based on fuzzy matching'
+  describe '#by_file_name' do
+    it 'finds file names based on fuzzy matching' do
+      expect(Noted::Search.by_file_name('foo')).to eq [
+        foo_dir + '/fool\'s errand.txt',
+        bar_dir + '/food.md',
+      ]
+    end
   end
 
-  describe '#full_text' do
-    it 'finds matching files by querying contents'
+  describe '#by_full_text' do
+    it 'finds matching files by querying contents' do
+      expect(Noted::Search.by_full_text('ba')).to eq [
+        bar_dir + '/food.md'
+      ]
+    end
   end
 
   describe '#relevance' do
